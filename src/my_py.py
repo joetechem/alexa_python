67bc0dfbd1ab9603e35e8267d7420d4c4172391e
joetechem <joe@techemstudios.com>
2017-08-31 17:06:49 -0400

    updating travis; changing responses from template

--- src/my_py.py
+++ src/my_py.py
@@ -21,13 +21,13 @@ import logging
 # --------------- Your functions to implement your intents ------------------
 
 def about():
-    return "<speak>Welcome to ACME Inc. We are the coolest company located in the Silicon Valley of the South. We love our employees, customers, and the environment.</speak>"
+    return "<speak>Welcome to Virginia Save Our Streams. A program of the Izaak Walton League of America. Virginia Save Our Streams monitors water quality of Virginia's streams and educates the public about the importance of clean water.</speak>"
 
 def contact():
-    return "<speak>The best way to reach us is at info at acme dot com. You can also leave us voice mail at 8 0 4, 5 5 5, 1 2 1 2. We are also on twitter, at acme S V O S.</speak>"
+    return "<speak>The best way to reach us is at info at V. A. S. O. S. dot org.</speak>"
 
 def upcoming():
-    return "<speak>Check us out at Stir Trek dot com. We will be in Columbus, Ohio on May  5th!</speak>"
+    return "<speak>Check us out at a stream near you. We will be in the water collecting data!</speak>"
 
 
 # --------------- Primary/Required functions (update as needed) ------------------
@@ -36,17 +36,17 @@ def launch():
     """ Called when the user launches the skill without specifying what they want
     """
 
-    return "<speak>Welcome to the 411 for ACME Inc. This skill provides information about ACME, a really cool company located in Silicon Valley of the South.</speak>"
+    return "<speak>Welcome to the 411 for Virginia Save Our Streams. This skill provides information about V. A. S. O S., a program of the Isaak Walton League of America.</speak>"
 
 
 def help():
     """ Called when the user asks for help
     """
 
-    return "<speak>This skill provides some basic information about ACME. You can ask for our location, contact info, and upcoming events.</speak>"
+    return "<speak>This skill provides some basic information about V. A. S. O. S. You can ask for our location, contact info, and upcoming events.</speak>"
 
 
 def end():
 
-    return "<speak.Thank you for asking about our business. Have a nice day! </speak>"
+    return "<speak.Thank you for asking about our organzization. Have a nice day! </speak>"
 
